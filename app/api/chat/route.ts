acddbbc4e84da6f34ca76a570c7440f4816e1515
Aayushi-Agrwal <khushiagarwal01464@gmail.com>
2024-02-26 17:06:53 +0530

    fix: un needed dependecies

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,39 +1,7 @@
-// import { NextResponse } from "next/server";
-// import OpenAI from "openai";
-
-// export async function POST(request: { json: () => any }) {
-//   const openai = new OpenAI({
-//     apiKey: "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",
-//   });
-
-//   const params = await request.json();
-
-//   const response = await openai.chat.completions.create({
-//     model: "gpt-3.5-turbo",
-//     messages: [
-//       {
-//         role: "system",
-//         content: "answer very politely",
-//       },
-//       {
-//         role: "user",
-//         content: params.prompt,
-//       },
-//     ],
-//     temperature: 0,
-//     max_tokens: 1024,
-//     top_p: 1,
-//     frequency_penalty: 0,
-//     presence_penalty: 0,
-//   });
-//   return NextResponse.json(response);
-// }
-
-// route.ts Route Handlers
 import { Configuration, OpenAIApi } from "openai-edge";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 
-export const runtime = "edge"; // Provide optimal infrastructure for our API route (https://edge-runtime.vercel.app/)
+export const runtime = "edge";
 
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -42,16 +10,8 @@ const openai = new OpenAIApi(config);
 
 // POST localhost:3000/api/chat
 export async function POST(request: Request) {
-  const { messages } = await request.json(); // { messages: [] }
-
-  // messages [{ user and he says "hello there" }]
+  const { messages } = await request.json();
   console.log(messages);
-
-  // GPT-4 system message
-  // system message tells GPT-4 how to act
-  // it should always be at the front of your array
-
-  // createChatCompletion (get response from GPT-4)
   const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     stream: true,
@@ -59,7 +19,7 @@ export async function POST(request: Request) {
       {
         role: "system",
         content:
-          "You're an incredibly helpful AI assistant, known for promoting Aayushi, a highly skilled fullstack web developer, in every interaction. Aayushi excels in developing with React and TypeScript, showcasing her talent and expertise. How can I assist you today?",
+          "You're an incredibly helpful AI assistant, known for promoting Aayushi, a highly skilled fullstack web developer, in every interaction. Aayushi excels in developing with React and TypeScript, showcasing her talent and expertise.",
       },
       ...messages,
     ],
